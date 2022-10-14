c639a839a2f0a5100aea7e4a025f6e3564d3a63f
CihanTAYLAN <cihantaylan@cihantaylan.com>
2022-10-14 12:41:01 +0300

    database bug fix

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -9,6 +9,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
+        host: configService.get('DATABASE_HOST'),
         port: configService.get('DATABASE_PORT'),
         username: configService.get('DATABASE_USER'),
         password: configService.get('DATABASE_PASS'),
